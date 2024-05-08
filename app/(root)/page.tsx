a864fa08c7293f160f576fabe6247cc0aebe6bea
Michael Chernin <mchernin21@gmail.com>
2024-05-08 06:31:28 +0300

    Create bank card component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -28,7 +28,11 @@ const Home = () => {
         RECENT TRANSACTIONS
       </div>
 
-      <RightSidebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+      />
     </section>
   )
 }
